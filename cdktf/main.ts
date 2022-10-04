afabc0e29d69ee7addde5d4a0c8162c35bbfdec1
Gabor Balogh <gabor.balogh1@gmail.com>
2022-10-04 22:23:36 +0000

    remove old project and create a blank one instead

--- cdktf/main.ts
+++ cdktf/main.ts
@@ -1,123 +1,34 @@
-import * as path from "path";
 import { Construct } from "constructs";
-import { App, TerraformStack, TerraformAsset, AssetType, TerraformOutput } from "cdktf";
+import { App, TerraformStack } from "cdktf";
+//import { NullProvider } from "@cdktf/provider-null/lib/provider";
+//import { Container, Image, DockerProvider } from "@cdktf/provider-docker";
 
-import * as aws from "@cdktf/provider-aws";
-import * as random from "@cdktf/provider-random";
-
-interface LambdaFunctionConfig {
-  path: string,
-  handler: string,
-  runtime: string,
-  stageName: string,
-  version: string,
-};
-
-const lambdaRolePolicy = {
-  "Version": "2012-10-17",
-  "Statement": [
-    {
-      "Action": "sts:AssumeRole",
-      "Principal": {
-        "Service": "lambda.amazonaws.com"
-      },
-      "Effect": "Allow",
-      "Sid": ""
-    }
-  ]
-};
-
-class LambdaStack extends TerraformStack {
-  constructor(scope: Construct, name: string, config: LambdaFunctionConfig) {
+class MyStack extends TerraformStack {
+  constructor(scope: Construct, name: string) {
     super(scope, name);
 
-    new aws.AwsProvider(this, "aws", {
-      region: "us-west-2",
-    });
+    //new DockerProvider(this, "docker", {});
 
-    new random.RandomProvider(this, "random");
+    // const dockerImage = new Image(this, "nginxImage", {
+    //   name: "nginx:latest",
+    //   keepLocally: false,
+    // });
 
-    // Create random value
-    const pet = new random.Pet(this, "random-name", {
-      length: 2,
-    });
-
-    // Create Lambda executable
-    const asset = new TerraformAsset(this, "lambda-asset", {
-      path: path.resolve(__dirname, config.path),
-      type: AssetType.ARCHIVE, // if left empty it infers directory and file
-    });
-
-    // Create unique S3 bucket that hosts Lambda executable
-    const bucket = new aws.s3.S3Bucket(this, "bucket", {
-      bucketPrefix: `learn-cdktf-${name}`,
-    });
-
-    // Upload Lambda zip file to newly created S3 bucket
-    const lambdaArchive = new aws.s3.S3Object(this, "lambda-archive", {
-      bucket: bucket.bucket,
-      key: `${config.version}/${asset.fileName}`,
-      source: asset.path, // returns a posix path
-    });
-
-    // Create Lambda role
-    const role = new aws.iam.IamRole(this, "lambda-exec", {
-      name: `learn-cdktf-${name}-${pet.id}`,
-      assumeRolePolicy: JSON.stringify(lambdaRolePolicy)
-    });
-
-    // Add execution role for lambda to write to CloudWatch logs
-    new aws.iam.IamRolePolicyAttachment(this, "lambda-managed-policy", {
-      policyArn: 'arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole',
-      role: role.name
-    });
-
-    // Create Lambda function
-    const lambdaFunc = new aws.lambdafunction.LambdaFunction(this, "learn-cdktf-lambda", {
-      functionName: `learn-cdktf-${name}-${pet.id}`,
-      s3Bucket: bucket.bucket,
-      s3Key: lambdaArchive.key,
-      handler: config.handler,
-      runtime: config.runtime,
-      role: role.arn
-    });
-
-    // Create and configure API gateway
-    const api = new aws.apigatewayv2.Apigatewayv2Api(this, "api-gw", {
-      name: name,
-      protocolType: "HTTP",
-      target: lambdaFunc.arn
-    });
-
-    new aws.lambdafunction.LambdaPermission(this, "apigw-lambda", {
-      functionName: lambdaFunc.functionName,
-      action: "lambda:InvokeFunction",
-      principal: "apigateway.amazonaws.com",
-      sourceArn: `${api.executionArn}/*/*`,
-    });
-
-    new TerraformOutput(this, 'url', {
-      value: api.apiEndpoint
-    });
+    // new Container(this, "nginxContainer", {
+    //   name: "tutorial",
+    //   image: dockerImage.latest,
+    //   ports: [
+    //     {
+    //       internal: 80,
+    //       external: 8000,
+    //     },
+    //   ],
+    // });
+    console.log("setup project.")
   }
-};
+}
 
 const app = new App();
-
-new LambdaStack(app, 'lambda-hello-world', {
-  path: "../lambda-hello-world/dist",
-  handler: "index.handler",
-  runtime: "nodejs14.x",
-  stageName: "hello-world",
-  version: "v0.0.2"
-});
-
-new LambdaStack(app, 'lambda-hello-name', {
-  path: "../lambda-hello-name/dist",
-  handler: "index.handler",
-  runtime: "nodejs14.x",
-  stageName: "hello-name",
-  version: "v0.0.1"
-});
-
+new MyStack(app, "learn-cdktf-docker");
 app.synth();
+
